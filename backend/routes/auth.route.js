7848f165f49b42ae8f44b8a62f11fff9c3a17e60
arya-prog <146481156+arya-prog@users.noreply.github.com>
2026-03-23 20:29:53 +0530

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,7 @@
 import express from "express";
 import { googleAuth, logout } from "../controllers/auth.controller.js";
-import { isAuth } from "../middlewares/isAuth.js";
 const authRouter = express.Router();
 
-authRouter.post("/google", isAuth, googleAuth);
+authRouter.post("/google", googleAuth);
 authRouter.post("/logout", logout);
 export default authRouter;
